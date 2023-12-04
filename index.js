12ecbb22fe76a5b9e8bbf24b8a02d227415069e0
Sumeet Makhija <sumeet.makhija.hp@gmail.com>
2023-12-04 22:16:46 +0530

    completed

--- index.js
+++ index.js
@@ -1,24 +1,49 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const validUrl = require('valid-url');
+
 const app = express();
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+let map = new Map();
+
+
+app.post('/api/shorturl', function (req, res) {
+  const url = req.body['url']
+  const isValid = validUrl.isHttpUri(url);
+  if (isValid) {
+    const number = Math.floor((Math.random() * 10000) + 1);
+    map.set(number.toString(), url);
+    return res.json({ original_url: url, short_url: number });
+  } else {
+    return res.json({ error: 'invalid url' });
+  }
 });
 
-app.listen(port, function() {
-  console.log(`Listening on port ${port}`);
+app.get('/api/shorturl/:shorturl', function (req, res) {
+  const number = req.params.shorturl;
+  const url = map.get(number);
+  if (url !== undefined) {
+    res.redirect(url);
+  } else {
+    return;
+  }
+});
+
+
+app.listen(port, function () {
+  console.log(`app listening on port ${port}`)
 });
